ae97d1fcaabcbf41658636f5d98ceca11314feaa
James Weah <jweah@aggies.ncat.edu>
2023-06-22 11:51:21 -0400

    Included logic for the user to choose an answer, move on to the next question, and become notified as to whether they selected the right choice

--- script.js
+++ script.js
@@ -13,9 +13,37 @@ var question7 = document.querySelector('.question7');
 var question8 = document.querySelector('.question8');
 var question9 = document.querySelector('.question9');
 var question10 = document.querySelector('.question10');
+var result = document.querySelector('.result');
+var result2 = document.querySelector('.result2');
 
 startBtn.addEventListener('click', function () {
   introPage.classList.add('disappear');
   question1.classList.remove('disappear');
   console.log('Hello');
+
+  const answer1 = (event) => {
+    var clickedValue = event.target.textContent;
+    //console.log(clickedValue);
+    if (clickedValue == '1. push()') {
+      question1.classList.add('disappear');
+      question2.classList.remove('disappear');
+      result.classList.remove('disappear');
+      setTimeout(() => {
+        result.classList.add('disappear');
+      }, 1000);
+    } else {
+      question1.classList.add('disappear');
+      question2.classList.remove('disappear');
+      result2.classList.remove('disappear');
+      setTimeout(() => {
+        result2.classList.add('disappear');
+      }, 1000);
+    }
+  };
+
+  question1.addEventListener('click', answer1);
 });
+
+var clear = setTimeout(() => {
+  result.classList.add('disappear');
+}, 2000);
