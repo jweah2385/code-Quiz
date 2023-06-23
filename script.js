440336e8d0a658e4f78bac83b75e4d0fd91b032f
James Weah <jweah@aggies.ncat.edu>
2023-06-22 22:46:21 -0400

    Completed the logic for half of the multiple choice questions

--- script.js
+++ script.js
@@ -19,12 +19,11 @@ var result2 = document.querySelector('.result2');
 startBtn.addEventListener('click', function () {
   introPage.classList.add('disappear');
   question1.classList.remove('disappear');
-  console.log('Hello');
 
   const answer1 = (event) => {
-    var clickedValue = event.target.textContent;
+    let clickedValue = event.target.textContent;
     //console.log(clickedValue);
-    if (clickedValue == '1. push()') {
+    if (clickedValue.trim() == '1. push()') {
       question1.classList.add('disappear');
       question2.classList.remove('disappear');
       result.classList.remove('disappear');
@@ -41,7 +40,97 @@ startBtn.addEventListener('click', function () {
     }
   };
 
+  const answer2 = (event) => {
+    let secondAns = event.target.textContent;
+    console.log(secondAns);
+    if (
+      secondAns.trim() == '4. It depends on the context in which it is used'
+    ) {
+      question2.classList.add('disappear');
+      question3.classList.remove('disappear');
+      result.classList.remove('disappear');
+      setTimeout(() => {
+        result.classList.add('disappear');
+      }, 1000);
+    } else {
+      question2.classList.add('disappear');
+      question3.classList.remove('disappear');
+      result2.classList.remove('disappear');
+      setTimeout(() => {
+        result2.classList.add('disappear');
+      }, 1000);
+    }
+  };
+
+  const answer3 = (event) => {
+    var thirdAns = event.target.textContent;
+    console.log(thirdAns);
+    if (thirdAns.trim() == '4. int a = 20;') {
+      question3.classList.add('disappear');
+      question4.classList.remove('disappear');
+      result.classList.remove('disappear');
+      setTimeout(() => {
+        result.classList.add('disappear');
+      }, 1000);
+    } else {
+      question3.classList.add('disappear');
+      question4.classList.remove('disappear');
+      result2.classList.remove('disappear');
+      setTimeout(() => {
+        result2.classList.add('disappear');
+      }, 1000);
+    }
+  };
+
+  const answer4 = (event) => {
+    var fourthAns = event.target.textContent;
+    console.log(fourthAns);
+    if (fourthAns.trim() == '1. push()') {
+      question4.classList.add('disappear');
+      question5.classList.remove('disappear');
+      result.classList.remove('disappear');
+      setTimeout(() => {
+        result.classList.add('disappear');
+      }, 1000);
+    } else {
+      question4.classList.add('disappear');
+      question5.classList.remove('disappear');
+      result2.classList.remove('disappear');
+      setTimeout(() => {
+        result2.classList.add('disappear');
+      }, 1000);
+    }
+  };
+
+  const answer5 = (event) => {
+    var fithAns = event.target.textContent;
+    if (fithAns.trim() == '1. "Not a Number"') {
+      question5.classList.add('disappear');
+      question6.classList.remove('disappear');
+      result.classList.remove('disappear');
+      setTimeout(() => {
+        result.classList.add('disappear');
+      }, 1000);
+    } else {
+      question5.classList.add('disappear');
+      question6.classList.remove('disappear');
+      result2.classList.remove('disappear');
+      setTimeout(() => {
+        result2.classList.add('disappear');
+      }, 1000);
+    }
+  };
+
   question1.addEventListener('click', answer1);
+  question2.addEventListener('click', answer2);
+  question3.addEventListener('click', answer3);
+  question4.addEventListener('click', answer4);
+  question5.addEventListener('click', answer5);
+  question5.addEventListener('click', answer6);
+  question5.addEventListener('click', answer7);
+  question5.addEventListener('click', answer8);
+  question5.addEventListener('click', answer9);
+  question5.addEventListener('click', answer10);
 });
 
 var clear = setTimeout(() => {
